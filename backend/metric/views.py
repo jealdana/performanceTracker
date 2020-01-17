d92b4b7b6ae5b1e6cce9c5b7f8a68da4eaa2cf16
jealdana <aldana.sigona@gmail.com>
2020-01-17 14:46:29 +0200

    create the view for the API to access all the metrics

--- backend/metric/views.py
+++ backend/metric/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import MetricSerializer      
+from .models import Metric                     
 
 # Create your views here.
+class MetricView(viewsets.ModelViewSet):
+  serializer_class = MetricSerializer
+  queryset = Metric.objects.all()
